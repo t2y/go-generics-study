a214541678aa526b74d758c9d12158c63658107e
Tetsuya Morimoto <tetsuya.morimoto@gmail.com>
2023-07-06 09:25:54 +0900

    update stringer

--- stringer/main.go
+++ stringer/main.go
@@ -8,12 +8,24 @@ func (h Hex) String() string {
 	return fmt.Sprintf("%x", int(h))
 }
 
-func dump[T fmt.Stringer](v T) {
+func dumpWithTypeParameter[T fmt.Stringer](v T) {
+	fmt.Printf("0x%s\n", v.String())
+}
+
+func dumpWithInterface(v fmt.Stringer) {
 	fmt.Printf("0x%s\n", v.String())
 }
 
 func main() {
-	dump(Hex(1))
-	dump(Hex(15))
-	dump(Hex(16))
+	// type parameter (static)
+	dumpWithTypeParameter(Hex(1))
+	dumpWithTypeParameter(Hex(15))
+	dumpWithTypeParameter(Hex(16))
+
+	fmt.Println("================================")
+
+	// interface (dynamic)
+	dumpWithInterface(Hex(1))
+	dumpWithInterface(Hex(15))
+	dumpWithInterface(Hex(16))
 }
